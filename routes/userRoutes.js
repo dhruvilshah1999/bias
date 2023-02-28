cae49b78df3d5111e19d0494342cdff6e4547a6c
dhruvilshah1999 <68918132+dhruvilshah1999@users.noreply.github.com>
2023-02-28 08:45:12 -0500

    - Notification, Add Employee, Add Appointment APIS
    - Created API and connected Front-End & Back-End for Add a New Employee
    - Created API and connected Front-End & Back-End for Notification
    - Created API and connected Front-End & Back-End for Add a Appointment
    - Other Back-end process were added like checking admin/employee/user login

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,10 @@ const {
   loginController,
   registerController,
   authController,
+  createEmployeeController,
+  createAppointmentController,
+  getAllNotificationController,
+  deleteAllNotificationController
 } = require("../controllers/userCtrl");
 const authMiddleware = require("../middleware/authMiddleware");
 
@@ -10,13 +14,25 @@ const authMiddleware = require("../middleware/authMiddleware");
 const router = express.Router();
 
 //routes
-//LOGIN || POST
+// LOGIN || POST
 router.post("/login", loginController);
 
-//REGISTER || POST
+// REGISTER || POST
 router.post("/register", registerController);
 
 // Authentication 
 router.post("/getAdminData", authMiddleware, authController)
+
+// Create Employee 
+router.post("/create-employee", authMiddleware, createEmployeeController);
+
+// Create Appointment create-appointment
+router.post("/create-appointment", authMiddleware, createAppointmentController);
+
+// Get all Notifications
+router.post("/get-all-notification", authMiddleware, getAllNotificationController);
+
+// Delete all Notifications
+router.post("/delete-all-notification", authMiddleware, deleteAllNotificationController);
 
 module.exports = router;
