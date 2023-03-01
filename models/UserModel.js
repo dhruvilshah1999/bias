8797d631301fa216d8973c9349f9c5c9f12aa8fa
nizamdiwan <52311458+nizamdiwan@users.noreply.github.com>
2023-03-01 08:45:12 -0500

    Working on validation for user details on login and signup page

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,31 +1,5 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "name is require"],
-  },
-  email: {
-    type: String,
-    required: [true, "email is require"],
-  },
-  password: {
-    type: String,
-    required: [true, "password is require"],
-  },
-  isAdmin: {
-    type: Boolean,
-    default: false,
-  },
-  notifcation: {
-    type: Array,
-    default: [],
-  },
-  seennotification: {
-    type: Array,
-    default: [],
-  },
-});
 
 const userModel = mongoose.model("users", userSchema);
 
